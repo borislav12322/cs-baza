ec44ed47e953cc92dbda946b8a69828e1f969eee
borislav12322 <borislavizmestiev@gmail.com>
2024-03-13 12:33:52 +0300

    first commit

---
+++ hw_03/index.js
@@ -0,0 +1,22 @@
+function* fizzBuzz() {
+    let i = 1n;
+    while (i <= 100n) {
+        switch (true) {
+            case i % 5n === 0n && i % 3n === 0n:
+                yield 'FizzBuzz'
+                break
+            case i % 3n === 0n:
+                yield 'Fizz'
+                break
+            case i % 5n === 0n:
+                yield 'Buzz'
+                break
+            default:
+                yield i
+        }
+
+        i++;
+    }
+}
+
+const fizzBuzzInstance = fizzBuzz();
