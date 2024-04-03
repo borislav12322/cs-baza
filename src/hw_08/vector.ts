90b4d7dbb8a5a61b632e908a9f9fea1437401434
borislav12322 <borislavizmestiev@gmail.com>
2024-04-03 10:59:54 +0300

    feat: vector

--- src/hw_08/vector.ts
+++ src/hw_08/vector.ts
@@ -15,13 +15,15 @@ class Vector {
   public length = 0;
   public buffer: ArrayBuffer;
   private ArrayConstructor: TArrayConstructor;
+  private initCapacity: number;
 
   constructor(TypedArray: TArrayConstructor, options: IVectorOptions) {
     this.ArrayConstructor = TypedArray;
     const buffer = new ArrayBuffer(options.capacity * TypedArray.BYTES_PER_ELEMENT);
     this.typedArray = new TypedArray(buffer);
     this.capacity = options.capacity;
-    this.buffer = buffer;
+    this.buffer = this.typedArray.buffer;
+    this.initCapacity = options.capacity;
   }
 
   public push(value: number) {
@@ -34,7 +36,7 @@ class Vector {
       const newTypedArray = new this.ArrayConstructor(newArrayBuffer);
       newTypedArray.set(this.typedArray);
       this.typedArray = newTypedArray;
-      this.buffer = newArrayBuffer;
+      this.buffer = this.typedArray.buffer;
     }
 
     this.typedArray[this.length++] = value;
@@ -52,25 +54,74 @@ class Vector {
       throw new Error('Empty');
     }
   }
+
+  public shrinkToFit() {
+    const initCapacity = this.initCapacity;
+    const shrunkBuffer = this.typedArray.slice(0, initCapacity);
+
+    this.buffer = shrunkBuffer;
+    this.length = initCapacity;
+    this.capacity = initCapacity;
+
+    return shrunkBuffer;
+  }
+
+  public values() {
+    let cursor = 0;
+    const length = this.length;
+    const typedArray = this.typedArray;
+
+    return {
+      [Symbol.iterator]() {
+        return this;
+      },
+
+      next() {
+        const currentCursor = cursor;
+        cursor = cursor + 1;
+
+        return { value: typedArray[currentCursor], done: currentCursor === length };
+      },
+    };
+  }
 }
 
 const vec = new Vector(Int32Array, { capacity: 4 });
 
-console.log(vec.push(1));
-console.log(vec.push(2));
-console.log(vec.push(3));
-console.log(vec.push(4));
-console.log(vec.push(5));
+vec.push(1);
+vec.push(2);
+vec.push(3);
+vec.push(4);
 
 console.log(vec.typedArray);
 console.log(vec.capacity);
 console.log(vec.length);
+console.log(vec.buffer);
 console.log(vec.buffer.byteLength);
 
 console.log(vec.pop());
 console.log(vec.pop());
 console.log(vec.pop());
 console.log(vec.pop());
-console.log(vec.pop());
+
+vec.push(1);
+vec.push(2);
+vec.push(3);
+vec.push(4);
+vec.push(5);
+
+const i = vec.values();
+
+console.log('length' + vec.length);
+
+for (const a of i) {
+  console.log('el ' + a);
+}
+
+vec.shrinkToFit();
 
 console.log(vec.typedArray);
+console.log(vec.capacity);
+console.log(vec.length);
+console.log(vec.buffer);
+console.log(vec.buffer.byteLength);
